015293e519455026c55aa7aa24870fd37aef0b8c
takuya2816 <taku0909.kishikishi@gmail.com>
2020-01-23 16:27:36 +0900

    changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+"""
+アプリのロジックを書いていく
+以前作ったモデルに情報を要求しテンプレートに渡す
+"""
+
 from django.shortcuts import render
 
 # Create your views here.
+# post_list関数の作成
+#  テンプレートを（色々なものを合わせて）組み立てる render という関数
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
