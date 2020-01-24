9b0213d8c90bd6879be805fcf86286bec09b10f8
takuya2816 <taku0909.kishikishi@gmail.com>
2020-01-25 00:14:07 +0900

    Added view and template for detailed blog post as well as CSS for site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 # URLのパターンが"views.post_list"の時
 urlpatterns = [
     path('', views.post_list, name='post_list'),  # view作成とURLの名前
-]
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+]  # <int:pk>にはpkの数字が入る
 
 # post_listと言う名前のview
--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@
 from django.shortcuts import render
 from .models import Post  #.は現在のディレクトリを示す
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 # requestを受け取りresponseを返す
 
@@ -18,3 +19,9 @@ def post_list(request):
     
     # requestを受けてrender関数でhtmlへクエリセットを組み合わせる
     return render(request, 'blog/post_list.html', {'posts': posts})  # {'名前':クエリセット}
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)  # pkにkeyが入っていない時PageNotFoundPageに飛ぶ
+    # post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
